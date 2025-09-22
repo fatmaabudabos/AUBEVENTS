036c8785c6d753f6599fd44ae8765d170061d4e5
fatmaabudabos <fatmaabudabos@gmail.com>
2025-09-22 16:46:40 +0300

    password management backend with bug fixes tests and also rate limiting

--- database/createDatabase.py
+++ database/createDatabase.py
@@ -1,6 +1,7 @@
 # RUN THIS FILE ONCE TO CREATE THE DATABASE AND TABLES!
 
+
 from sqlmodel import SQLModel
-from database.database import get_engine
+from .database import get_engine
 
 SQLModel.metadata.create_all(get_engine())
