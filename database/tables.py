bfa281cd677201bbc8c4b3b40504192d51d615d9
Mhmd2oo5 <151791445+Mhmd2oo5@users.noreply.github.com>
2025-09-28 19:10:05 +0300

    Added Events and UserEventLink tables

--- database/tables.py
+++ database/tables.py
@@ -1,18 +1,37 @@
 # List of tables for the database
 
-from sqlmodel import SQLModel, Field
-from typing import Optional
+from sqlmodel import SQLModel, Field, Relationship
+from typing import Optional, List
 from datetime import datetime
 
+class UserEventLink(SQLModel, table=True):
+    user_email: str = Field(foreign_key="users.email", primary_key=True)
+    event_id: int = Field(foreign_key="events.id", primary_key=True)
+
 class Users(SQLModel, table=True):
-    email: str = Field(primary_key = True)
+    email: str = Field(primary_key=True)
     password_hash: str
 
     is_admin: bool = Field(default=False)
 
-    is_verified: bool = Field(default=False, nullable = False)
+    is_verified: bool = Field(default=False)
     verification_token: Optional[str] = Field(default=None)
     verification_token_expiry: Optional[datetime] = Field(default=None)
 
     reset_code: Optional[str] = Field(default=None)
     reset_code_expiry: Optional[datetime] = Field(default=None)
+
+    events: List["Events"] = Relationship(back_populates="users", link_model=UserEventLink)
+
+class Events(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    title: str
+    description: Optional[str] = Field(default=None)
+
+    date: Optional[datetime] = Field(default=None)
+    location: Optional[str] = Field(default=None)
+
+    capacity: Optional[int] = Field(default=None)
+    available_seats: Optional[int] = Field(default=None)
+
+    users: List[Users] = Relationship(back_populates="events", link_model=UserEventLink)
